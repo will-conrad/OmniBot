5950b18818ff829cccde3f2ec063157b69e277f8
Will Conrad <jwconrad23@gmail.com>
2021-03-19 15:34:50 -0400

    Updated for laser autonomy, degree/inch accurate movement

--- include/robot-config.h
+++ include/robot-config.h
@@ -2,25 +2,16 @@ using namespace vex;
 
 extern brain Brain;
 
-using signature = vision::signature;
-
 // VEXcode devices
 extern controller Controller1;
 extern motor North1;
 extern motor South2;
 extern motor West3L;
 extern motor East4R;
-extern signature Vision13__ORANGECUBE;
-extern signature Vision13__PURPLECUBE;
-extern signature Vision13__GREENCUBE;
-extern signature Vision13__SIG_4;
-extern signature Vision13__SIG_5;
-extern signature Vision13__SIG_6;
-extern signature Vision13__SIG_7;
-extern vision Vision13;
-extern distance Laser;
+extern distance LaserL;
 extern line BorderDetector;
 extern bumper StopButton;
+extern distance LaserR;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,10 @@
 // South2               motor         2               
 // West3L               motor         3               
 // East4R               motor         4               
-// Vision13             vision        13              
-// Laser                distance      10              
+// LaserL               distance      10              
 // BorderDetector       line          B               
 // StopButton           bumper        C               
+// LaserR               distance      11              
 // ---- END VEXCODE CONFIGURED DEVICES ----
 #include<vex.h>
 #include<iostream>
@@ -17,18 +17,19 @@
 using namespace vex;
 // ---- Initialization ---- //
 void spinMotors(); 
-void updateVelocity(int n);
+void updateVelocity(int n, bool useIn);
 void eBrake();
 void unbrake();
+float LaserAvg(distanceUnits units);
 void checkLine();
 void checkObject();
 void updateConsole();
-void turnaround();
+bool degMove(int dir, float in, float deg, int v);
 void screenColor(color c);
 
 int Brain_precision = 0, Console_precision = 0, Controller1_precision = 0, Vision13_objectIndex = 0;
 float northV, southV, eastV, westV, stickRotate, stickForward, stickSideways, objectLoc, autoTurnSpeed, autoFollowSpeed, autoSensitivity, charge;
-bool useController, autonomous, running, objectExists, objLeft, braking, atLine, atObject, seeObject, brakeMem, autoMem, lineMem, objMem;
+bool useController, autonomous, running, objectExists, objLeft, braking, atLine, nearObject, seeObject, brakeMem, autoMem, lineMem, objMem;
 
 // ---- FUNCTIONS ---- //
 void spinMotors() {
@@ -38,28 +39,28 @@ void spinMotors() {
   West3L.spin(forward);
   East4R.spin(forward);
 }
-void updateVelocity(int n) {
+void updateVelocity(int n, bool useIn) {
   //Calculate correct wheel speeds
   northV = (stickForward + stickSideways) + stickRotate;
   eastV = (stickForward - stickSideways) - stickRotate;
   westV = (stickForward - stickSideways) + stickRotate;
   southV = (stickForward + stickSideways) - stickRotate;
   //Set velocity
-  if (n == 1) {
+  if (!useIn) { //!useIn = use controller input
     North1.setVelocity(northV, percent);
     South2.setVelocity(southV, percent);
     West3L.setVelocity(westV, percent);
     East4R.setVelocity(eastV, percent);
-    spinMotors();
+    
   }
-  else if (n > 1) {
+  else if (useIn && n != 0) {
     North1.setVelocity(n, percent);
     South2.setVelocity(n, percent);
     West3L.setVelocity(n, percent);
     East4R.setVelocity(n, percent);
-    spinMotors();
+    
   }
-  else {
+  else if (useIn && n == 0) {
     North1.stop();
     South2.stop();
     West3L.stop();
@@ -67,7 +68,7 @@ void updateVelocity(int n) {
   }
 }
 void eBrake() { //
-  updateVelocity(0);
+  updateVelocity(0, true); //Halt
   North1.setStopping(hold);
   South2.setStopping(hold);
   West3L.setStopping(hold);
@@ -79,6 +80,9 @@ void unbrake() {
   West3L.setStopping(coast);
   East4R.setStopping(coast);
 }
+float laserAvg(distanceUnits units) {
+  return (LaserL.objectDistance(units) + LaserR.objectDistance(units)) / 2;
+}
 void checkLine() {
   if (BorderDetector.reflectivity() <= 10) {
     atLine = true;
@@ -88,15 +92,18 @@ void checkLine() {
   }
 }
 void checkObject() {
-  if (Laser.objectDistance(mm) < 100.0 && Laser.objectDistance(mm) > 0.1) {
-    atObject = true;
+  if (laserAvg(mm) > 1 && laserAvg(mm) < 100) {
+    nearObject = true;
   }
   else {
-    atObject = false;
+    nearObject = false;
   }
 }
+bool laserInRange(int range, distanceUnits units) {
+  if (LaserL.objectDistance(units) <= range && )
+}
 void updateConsole() {
-  if (braking != brakeMem || autonomous != autoMem || atLine != lineMem || atObject != objMem) {
+  if (braking != brakeMem || autonomous != autoMem || atLine != lineMem || nearObject != objMem) {
     Brain.Screen.clearScreen(); //Clear Text
     Brain.Screen.setCursor(1, 1);
     Brain.Screen.setFillColor(black);
@@ -121,32 +128,73 @@ void updateConsole() {
       Brain.Screen.print("At Line: False\n");
     }
     Brain.Screen.newLine();
-    if (atObject) {
-      Brain.Screen.print("At Object: True\n");
+    if (nearObject) {
+      Brain.Screen.print("Near Object: True\n");
     }
     else {
-      Brain.Screen.print("At Object: False\n");
+      Brain.Screen.print("Near Object: False\n");
     }
 
     brakeMem = braking;
     autoMem = autonomous;
     lineMem = atLine;
-    objMem = atObject;
+    objMem = nearObject;
   }
 }
-void turnaround() {
-  updateVelocity(50);
-  float wheelDeg = 645.0;
-  North1.spinFor(wheelDeg, degrees, false);
-  South2.spinFor(wheelDeg * -1, degrees, false);
-  East4R.spinFor(wheelDeg * -1, degrees, false);
-  West3L.spinFor(wheelDeg, degrees, true);
-}
+
 void screenColor(color c) {
   Brain.Screen.setFillColor(c);
   Brain.Screen.drawRectangle(270, 0, 210, 272);
   updateConsole();
 }
+
+bool degMove(int dir, float in, float deg, int v) {
+  updateVelocity(v, true);
+  if (dir == 1) { //Forward
+    float wheelDeg = in / 0.05;
+    North1.spinFor(wheelDeg, degrees, false);
+    South2.spinFor(wheelDeg, degrees, false);
+    West3L.spinFor(wheelDeg, degrees, false);
+    East4R.spinFor(wheelDeg, degrees, true);
+  }
+  else if (dir == 2) { //Backward
+    float wheelDeg = in / -0.05;
+    North1.spinFor(wheelDeg, degrees, false);
+    South2.spinFor(wheelDeg, degrees, false);
+    West3L.spinFor(wheelDeg, degrees, false);
+    East4R.spinFor(wheelDeg, degrees, true);
+  }
+  else if (dir == 3) { //Left
+    float wheelDeg = in / 0.05;
+    North1.spinFor(wheelDeg * -1, degrees, false);
+    South2.spinFor(wheelDeg * -1, degrees, false);
+    West3L.spinFor(wheelDeg, degrees, false);
+    East4R.spinFor(wheelDeg, degrees, true);
+  }
+  else if (dir == 4) { //Right
+    float wheelDeg = in / 0.05;
+    North1.spinFor(wheelDeg, degrees, false);
+    South2.spinFor(wheelDeg, degrees, false);
+    West3L.spinFor(wheelDeg * -1, degrees, false);
+    East4R.spinFor(wheelDeg * -1, degrees, true);
+  }
+  else if (dir == 5) { //Clockwise
+    float wheelDeg = deg / 0.279;
+    North1.spinFor(wheelDeg, degrees, false);
+    South2.spinFor(wheelDeg * -1, degrees, false);
+    East4R.spinFor(wheelDeg * -1, degrees, false);
+    West3L.spinFor(wheelDeg, degrees, true);
+  }
+  else if (dir == 6) { //Counter-Clockwise
+    float wheelDeg = deg / 0.279;
+    North1.spinFor(wheelDeg * -1, degrees, false);
+    South2.spinFor(wheelDeg, degrees, false);
+    East4R.spinFor(wheelDeg, degrees, false);
+    West3L.spinFor(wheelDeg * -1, degrees, true);
+  }
+  return true;
+}
+
 // ===========================================================================================================
 
 // ---- MAIN DRIVE CONTROL ---- //
@@ -158,7 +206,7 @@ int omniControl() {
   charge = 50;
   running = true;
   atLine = false;
-  atObject = false;
+  nearObject = false;
   
   autonomous = false;
   //Forever
@@ -174,21 +222,22 @@ int omniControl() {
     if (!braking) {
       unbrake();
       if (autonomous) { //If bot is in auto mode
-        if (!atLine && !atObject) { //NOT at line
+        
+        if (!atLine && !nearObject) { //NOT at line
           screenColor(orange); //Orange if autonomous
 
-          if ((Vision13.objectCount > 0 && Vision13.objectCount < 3)) { //Sees object
+          if (laserInRange(750, mm)) { //Sees object
             // || (Laser.objectDistance(mm) < 700 && Laser.objectDistance(mm) > 2)
             seeObject = true;
             objectLoc = ((Vision13.objects[Vision13_objectIndex].centerX - 157.5) / autoSensitivity) * -1.0; //Set relative object position
             //objectLoc = 0;
             stickRotate = objectLoc;
             
-            if (objectLoc > 0.0) { //Determine where object is
-              objLeft = false;
-            }
-            else {
+            if (LaserL.objectDistance(mm) <= 700 && (LaserR.objectDistance(mm) > 700 || LaserR.isObjectDetected() == false)) { //Determine where object is
               objLeft = true;
+            }
+            else if (LaserR.objectDistance(mm) <= 700 && (LaserL.objectDistance(mm) > 700 || LaserL.isObjectDetected() == false)){
+              objLeft = false;
             }
             stickForward = autoFollowSpeed; //Track object
           }
@@ -205,24 +254,22 @@ int omniControl() {
         }
         else if (atLine) {   //AT LINE
           screenColor(white); //White
-          updateVelocity(0);
-          turnaround();
-          wait(500, msec);
-          North1.spinFor(45, degrees, false);
-          South2.spinFor(-45, degrees, false);
-          East4R.spinFor(-45, degrees, false);
-          West3L.spinFor(45, degrees, true);
-         }
-        else if (atObject && charge > 1) {  //AT OBJECT  + CHARGE
-          screenColor(purple); //Purple
-          updateVelocity(charge); //Set vel
-          if (BorderDetector.reflectivity() <= 10) {
-            screenColor(yellow);
+          degMove(5, 0, 180, 50); //Spin 180deg 
+          degMove(1, 7, 0, 40); //Move forward 7 in
+        }
+        else if (nearObject && charge > 1) {  //AT OBJECT  + CHARGE
+          if (BorderDetector.reflectivity() <= 10) { //Check at line
+            screenColor(white);
             braking = true;
           }
+          else {
+            screenColor(purple); //Purple
+            updateVelocity(charge, true); //Set vel
+            spinMotors();
+          }
         }
-        else if (atObject && charge == 0) {  // AT OBJECT
-          updateVelocity(0);
+        else if (nearObject && charge == 0) {  // AT OBJECT NO CHARGE
+          updateVelocity(0, true); //Stop
           braking = true;
         }
       }
@@ -232,8 +279,9 @@ int omniControl() {
         stickForward = Controller1.Axis3.position();
         stickSideways = Controller1.Axis4.position();
       }
-      if (!atObject) {
-        updateVelocity(1); //Update drive normally if not charging
+      if (!nearObject) {
+        updateVelocity(1, false); //Update drive normally if not charging
+        spinMotors();
       }
     }
     else if (braking) { //Brake if braking == true
@@ -266,6 +314,6 @@ int main() {
 
   wait(15, msec);
   
-  screenColor(green);
+  updateConsole();
   omniControl();
 }
